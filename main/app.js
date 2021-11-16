71682a08c2c8942d7c33d5b096aaa0153dd9a6ee
roxanabalan99 <75169508+roxanabalan99@users.noreply.github.com>
2021-11-16 06:26:35 +0200

    Update app.js

--- main/app.js
+++ main/app.js
@@ -1,7 +1,37 @@
 const distance = (a, b) => {
-	//TODO: implementați funcția
-	// TODO: implement the function
-}
+  if (!a && !b) return 0;
 
+  if (
+    !(typeof a === "string" || a instanceof String) ||
+    !(typeof b === "string" || b instanceof String)
+  ) {
+    throw new Error("InvalidType");
+  }
 
-module.exports.distance = distance
+  var matrix = [];
+
+  for (let i = 0; i < a.length + 1; i++) {
+    var rows = [];
+    rows[0] = i;
+    for (let j = 1; j < b.length + 1; j++) {
+      rows.push(j);
+    }
+    matrix.push(rows);
+  }
+
+  for (let i = 1; i < a.length + 1; i++) {
+    for (let j = 1; j < b.length + 1; j++) {
+      if (a[i - 1] === b[j - 1]) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] =
+          Math.min(matrix[i - 1][j], matrix[i - 1][j - 1], matrix[i][j - 1]) +
+          1;
+      }
+    }
+  }
+
+  return matrix[a.length][b.length];
+};
+
+module.exports.distance = distance;
